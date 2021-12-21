56806b4a59b10f24488f5260c2d7a32a74808201
Jacob Gadikian <jacobgadikian@gmail.com>
2021-12-21 08:27:14 +0000

    Format code with prettier and standardjs

--- src/app/components/modules/Transfer.jsx
+++ src/app/components/modules/Transfer.jsx
@@ -790,8 +790,10 @@ export default connect(
                 memo: toVesting ? undefined : memo ? memo : '',
             };
 
-            let size = JSON.stringify(operation).replace(/[\[\]\,\"]/g, '')
-                .length;
+            let size = JSON.stringify(operation).replace(
+                /[\[\]\,\"]/g,
+                ''
+            ).length;
             let bw_fee = Math.max(
                 0.001,
                 ((size / 1024) * bandwidthKbytesFee).toFixed(3)
@@ -826,8 +828,10 @@ export default connect(
                         ' ' +
                         asset2,
                 };
-                let size = JSON.stringify(operation).replace(/[\[\]\,\"]/g, '')
-                    .length;
+                let size = JSON.stringify(operation).replace(
+                    /[\[\]\,\"]/g,
+                    ''
+                ).length;
                 let bw_fee = Math.max(
                     0.001,
                     ((size / 1024) * bandwidthKbytesFee).toFixed(3)

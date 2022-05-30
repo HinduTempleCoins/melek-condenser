65128608b06eb20e93c1281b4a61451139983a86
Jacob Gadikian <jacobgadikian@gmail.com>
2022-05-30 12:08:22 +0000

    Format code with prettier and standardjs

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,85 +1,86 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('config');
-const db = {};
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const basename = path.basename(module.filename)
+const env = process.env.NODE_ENV || 'development'
+const config = require('config')
+const db = {}
 
-const sequelize = env === 'development'? new Sequelize('sqlite::memory:') : new Sequelize(config.get('database_url'));
+const sequelize =
+    env === 'development'
+      ? new Sequelize('sqlite::memory:')
+      : new Sequelize(config.get('database_url'))
 
 fs.readdirSync(__dirname)
-    .filter((file) => {
-        return (
-            file.indexOf('.') !== 0 &&
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 &&
             file !== basename &&
             file.slice(-3) === '.js'
-        );
-    })
-    .forEach((file) => {
-        // const model = sequelize.import(path.join(__dirname, file));
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize);
-        db[model.name] = model;
-    });
+    )
+  })
+  .forEach((file) => {
+    // const model = sequelize.import(path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize)
+    db[model.name] = model
+  })
 
 Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
-});
+  if (db[modelName].associate) {
+    db[modelName].associate(db)
+  }
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
 if (env === 'development') {
-    // in dev, sync all table schema automatically for convenience
-    // (async()=>{
-    //    await sequelize.sync();
-    // })
-    sequelize.sync()
-        .then(() => console.log('DB sync done'))
-
+  // in dev, sync all table schema automatically for convenience
+  // (async()=>{
+  //    await sequelize.sync();
+  // })
+  sequelize.sync().then(() => console.log('DB sync done'))
 }
 
-function esc(value, max_length = 256) {
-    if (!value) return '';
-    if (typeof value === 'number') return value;
-    if (typeof value === 'boolean') return value;
-    if (typeof value !== 'string') return '(object)';
-    const res = value
-        .substring(0, max_length - max_length * 0.2)
-        .replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, (char) => {
-            switch (char) {
-                case '\0':
-                    return '\\0';
-                case '\x08':
-                    return '\\b';
-                case '\x09':
-                    return '\\t';
-                case '\x1a':
-                    return '\\z';
-                case '\n':
-                    return '\\n';
-                case '\r':
-                    return '\\r';
+function esc (value, max_length = 256) {
+  if (!value) return ''
+  if (typeof value === 'number') return value
+  if (typeof value === 'boolean') return value
+  if (typeof value !== 'string') return '(object)'
+  const res = value
+    .substring(0, max_length - max_length * 0.2)
+    .replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, (char) => {
+      switch (char) {
+        case '\0':
+          return '\\0'
+        case '\x08':
+          return '\\b'
+        case '\x09':
+          return '\\t'
+        case '\x1a':
+          return '\\z'
+        case '\n':
+          return '\\n'
+        case '\r':
+          return '\\r'
                 // case '\'':
                 // case "'":
                 // case '"':
                 // case '\\':
                 // case '%':
                 //     return '\\' + char; // prepends a backslash to backslash, percent, and double/single quotes
-            }
-            return '-';
-        });
-    return res.length < max_length ? res : '-';
+      }
+      return '-'
+    })
+  return res.length < max_length ? res : '-'
 }
 
-db.esc = esc;
+db.esc = esc
 
 db.escAttrs = function (attrs) {
-    const res = {};
-    Object.keys(attrs).forEach((key) => (res[key] = esc(attrs[key])));
-    return res;
-};
+  const res = {}
+  Object.keys(attrs).forEach((key) => (res[key] = esc(attrs[key])))
+  return res
+}
 
-module.exports = db;
+module.exports = db
--- src/shared/RootSaga.js
+++ src/shared/RootSaga.js
@@ -1,20 +1,20 @@
-import { all } from 'redux-saga/effects';
-import { fetchDataWatches } from 'app/redux/FetchDataSaga';
-import { sharedWatches } from 'app/redux/SagaShared';
-import { userWatches } from 'app/redux/UserSaga';
-import { authWatches } from 'app/redux/AuthSaga';
-import { communityWatches } from 'app/redux/CommunitySaga';
-import { transactionWatches } from 'app/redux/TransactionSaga';
-import { proposalWatches } from 'app/redux/ProposalSaga';
+import { all } from 'redux-saga/effects'
+import { fetchDataWatches } from 'app/redux/FetchDataSaga'
+import { sharedWatches } from 'app/redux/SagaShared'
+import { userWatches } from 'app/redux/UserSaga'
+import { authWatches } from 'app/redux/AuthSaga'
+import { communityWatches } from 'app/redux/CommunitySaga'
+import { transactionWatches } from 'app/redux/TransactionSaga'
+import { proposalWatches } from 'app/redux/ProposalSaga'
 
-export default function* rootSaga() {
-    yield all([
-        ...userWatches, // keep first to remove keys early when a page change happens
-        ...fetchDataWatches,
-        ...sharedWatches,
-        ...authWatches,
-        ...transactionWatches,
-        ...proposalWatches,
-        ...communityWatches,
-    ]);
+export default function * rootSaga () {
+  yield all([
+    ...userWatches, // keep first to remove keys early when a page change happens
+    ...fetchDataWatches,
+    ...sharedWatches,
+    ...authWatches,
+    ...transactionWatches,
+    ...proposalWatches,
+    ...communityWatches
+  ])
 }

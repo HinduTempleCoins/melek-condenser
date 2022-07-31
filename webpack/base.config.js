3b6be1df5c46036411737e133ab9e20de0c01016
Samuel Alphée Richard <6863059-Saboin@users.noreply.gitlab.com>
2022-07-31 17:28:59 -0300

    wallet - fixed filenaming based on content hash for css in webpack

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -66,8 +66,8 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: '[name].[hash].js',
-        chunkFilename: '[id].[hash].js',
+        filename: `[name].[contenthash].js`,
+        chunkFilename: `[id].[contenthash].js`,
         publicPath: '/assets/',
     },
     module: {
@@ -120,7 +120,7 @@ module.exports = {
             this.plugin('done', writeStats);
         },
         webpack_isomorphic_tools_plugin,
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin({filename: `[name]-[contenthash].css`})
     ],
     resolve: {
         alias: {
